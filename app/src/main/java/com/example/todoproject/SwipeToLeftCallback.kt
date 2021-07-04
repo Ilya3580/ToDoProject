0d0c7ec51e4ec15d236e22b72b3f4127570de093
user <ilya.smetanin.2002@mail.ru>
2021-07-04 14:45:57 +0300

    3

---
+++ app/src/main/java/com/example/todoproject/SwipeToLeftCallback.kt
@@ -0,0 +1,102 @@
+package com.example.todoproject
+
+import android.content.Context
+import android.graphics.*
+import android.graphics.drawable.ColorDrawable
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.abs
+
+class SwipeToLeftCallback(context: Context, var adapter : ItemTouchHelperAdapter) :
+    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+    private val deleteIcon = ContextCompat.getDrawable(context, R.drawable.ic_delete)
+    private val intrinsicWidth = deleteIcon?.intrinsicWidth
+    private val intrinsicHeight = deleteIcon?.intrinsicHeight
+    private val background = ColorDrawable()
+    private val backgroundColor = ContextCompat.getColor(context, R.color.Red)
+
+    override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+        return false
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        adapter.onSwipeLeft(viewHolder.adapterPosition)
+    }
+
+    override fun onChildDraw(
+        c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+        dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean
+    ) {
+
+        val itemView = viewHolder.itemView
+        val itemHeight = itemView.bottom - itemView.top
+        val itemWidth = abs(dX)
+
+        background.color = backgroundColor
+        background.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+        background.draw(c)
+
+        val deleteIconTop = itemView.top + (itemHeight - intrinsicHeight!!) / 2
+        val deleteIconMargin = (itemWidth - intrinsicWidth!!).toInt() / 2
+        val deleteIconLeft = itemView.right - deleteIconMargin - intrinsicWidth
+        val deleteIconRight = itemView.right - deleteIconMargin
+        val deleteIconBottom = deleteIconTop + intrinsicHeight
+
+        deleteIcon?.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom)
+        deleteIcon?.draw(c)
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+
+}
+
+class SwipeToRightCallback(context: Context, var adapter : ItemTouchHelperAdapter) :
+    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+
+    private val checkIcon = ContextCompat.getDrawable(context, R.drawable.ic_check)
+    private val intrinsicWidth = checkIcon?.intrinsicWidth
+    private val intrinsicHeight = checkIcon?.intrinsicHeight
+    private val background = ColorDrawable()
+    private val backgroundColor = ContextCompat.getColor(context, R.color.Green)
+
+    override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+        return false
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        adapter.onSwipeRight(viewHolder.adapterPosition)
+    }
+
+    override fun onChildDraw(
+        c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+        dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean
+    ) {
+
+        val itemView = viewHolder.itemView
+        val itemHeight = itemView.bottom - itemView.top
+        val itemWidth = abs(dX)
+
+        background.color = backgroundColor
+        background.setBounds(itemView.left + dX.toInt(), itemView.top, itemView.left, itemView.bottom)
+        background.draw(c)
+
+        val checkIconTop = itemView.top + (itemHeight - intrinsicHeight!!) / 2
+        val checkIconMargin = (itemWidth - intrinsicWidth!!).toInt() / 2
+        val checkIconLeft = itemView.left + checkIconMargin
+        val checkIconRight = itemView.left + checkIconMargin + intrinsicWidth
+        val checkIconBottom = checkIconTop + intrinsicHeight
+
+        checkIcon?.setBounds(checkIconLeft, checkIconTop, checkIconRight, checkIconBottom)
+        checkIcon?.draw(c)
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+
+}
+
+interface ItemTouchHelperAdapter{
+    fun onSwipeLeft(position : Int)
+    fun onSwipeRight(position: Int)
+}
